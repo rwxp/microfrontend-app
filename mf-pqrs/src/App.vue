99a60370f30f2ce44f05d55290309ec20a126889
Santiagospitia <ospitia2002@gmail.com>
2023-11-22 22:43:03 -0500

    pqrs

---
+++ mf-pqrs/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <PQRS />
+</div>
+</template>
+
+<script>
+import PQRS from './components/PQRS.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PQRS
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
